7d3e126f62e50ec5b654cd20a08334c128e4f497
Stephen Mathieson <me@stephenmathieson.com>
2019-02-14 14:14:40 -0500

    fmt

--- .circleci/canary-version.js
+++ .circleci/canary-version.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const pkg = require('../package.json');
+const fs = require('fs')
+const pkg = require('../package.json')
 
 if (!process.env.CIRCLE_SHA1) {
-  throw new Error('No CIRCLE SHA available.');
+  throw new Error('No CIRCLE SHA available.')
 }
 
-const next = pkg.version + '-canary.' + process.env.CIRCLE_SHA1.substring(0, 8);
-pkg.version = next;
-fs.writeFileSync('./package.json', JSON.stringify(pkg, null, 2));
+const next = pkg.version + '-canary.' + process.env.CIRCLE_SHA1.substring(0, 8)
+pkg.version = next
+fs.writeFileSync('./package.json', JSON.stringify(pkg, null, 2))
